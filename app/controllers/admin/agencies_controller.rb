e2cf3c5a1a8925e5df8ae40cfcab64f655340b48
zainudinsainal <supernova37@gmail.com>
2018-02-28 21:21:00 +0800

    Add seeds file and admin::agencies controller

---
+++ app/controllers/admin/agencies_controller.rb
@@ -0,0 +1,2 @@
+class Admin::AgenciesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   resources :agencies
+
+  namespace :admin do
+    resources :agencies
+    root "agencies#index"
+  end
 end
