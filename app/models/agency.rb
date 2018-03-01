148b97abe4c97ea35af3bb585452b09a24aeeff9
zainudinsainal <supernova37@gmail.com>
2018-03-01 12:41:48 +0800

    Update enum category in agency model

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -2,5 +2,5 @@ class Agency < ApplicationRecord
 
   has_many :blocks
 
-  enum category: [:Financial, :Teens, :Disabilty]
+  enum category: [:financial, :family, :food_rations, :donations, :health, :elderly, :children, :teens, :unemployment, :homeless, :mental_health, :addictions, :special_needs, :disability]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :agencies
-    
   end
 
   root to: "admin/agencies#index"
