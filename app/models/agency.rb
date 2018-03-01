92aaabe77368638714aaba9504727357b54a2620
kommune <kommune.000@gmail.com>
2018-03-01 12:47:40 +0800

    Added SSO Halfway Houses and Mental Health

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,4 +1,7 @@
 class Agency < ApplicationRecord
 
   has_many :blocks
+
+  enum category: [:financial, :family, :food_rations, :donations, :health, :elderly, :children, :teens, :unemployment, :homeless, :mental_health, :addictions, :special_needs, :disability]
+  
 end
